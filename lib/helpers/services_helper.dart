048ae9acc9ffc0870906f7f83ee0e5862e758ec6
MateusSJames <mateusaristides23@gmail.com>
2022-10-29 11:08:11 -0300

    Implementando os metodos de task service

--- lib/helpers/services_helper.dart
+++ lib/helpers/services_helper.dart
@@ -2,6 +2,6 @@ abstract class ServicesHelper<TReturn, TParam> {
   Future<int> insert(TParam param);
   Future<int> update(TParam param);
   Future<int> delete(int id);
-  Future<TReturn> getAll();
-  Future<TReturn> getById(int id);
+  Future<List<TReturn>> getAll();
+  Future<TReturn?> getById(int id);
 }
--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,3 +1,66 @@
-class TaskService {
-  
+import 'package:agenda/connections/db_connection.dart';
+import 'package:agenda/helpers/services_helper.dart';
+import 'package:agenda/models/tasks.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TaskService implements ServicesHelper<Tasks, Tasks> {
+  final _databaseConnection = DatabaseConnection();
+  @override
+  Future<int> delete(int id) async {
+    Database _db = await _databaseConnection.database;
+    var result = await _db.delete(
+      _databaseConnection.taskTable,
+      where: '${_databaseConnection.idTask} = ?',
+      whereArgs: [id],
+    );
+    return result;
+  }
+
+  @override
+  Future<List<Tasks>> getAll() async {
+    Database _db = await _databaseConnection.database;
+    var result = await _db.query(
+      _databaseConnection.taskTable,
+    );
+    List<Tasks> tasks =
+        result.isNotEmpty ? result.map((e) => Tasks.fromJson(e)).toList() : [];
+    return tasks;
+  }
+
+  @override
+  Future<Tasks?> getById(int id) async {
+    Database _db = await _databaseConnection.database;
+    List<Map<String, dynamic>> values = await _db.query(
+      _databaseConnection.taskTable,
+      where: '${_databaseConnection.idTask} = ?',
+      whereArgs: [id],
+    );
+    if (values.isNotEmpty) {
+      return Tasks.fromJson(values.first);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<int> insert(Tasks task) async {
+    Database _db = await _databaseConnection.database;
+    var result = await _db.insert(
+      _databaseConnection.taskTable,
+      task.toJson(),
+    );
+    return result;
+  }
+
+  @override
+  Future<int> update(Tasks task) async {
+    Database _db = await _databaseConnection.database;
+    var result = await _db.update(
+      _databaseConnection.taskTable,
+      task.toJson(),
+      where: '${_databaseConnection.idTask} = ?',
+      whereArgs: [task.id],
+    );
+    return result;
+  }
 }
